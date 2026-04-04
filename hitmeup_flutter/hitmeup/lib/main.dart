9326b2b82f61b20c144f9288a71f0e4a0f3d46b2
nerd-emoji <nathaniellouish@gmail.com>
2026-04-04 19:05:37 +0700

    small fix again

--- hitmeup_flutter/hitmeup/lib/main.dart
+++ hitmeup_flutter/hitmeup/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/splash_screen.dart';
-import 'screens/auth/verification_code_screen.dart';
 import 'screens/auth/verification_success_screen.dart';
 import 'theme/app_theme.dart';
 
